b85e54fab9f011afdbc596514b953c26e5d80ecf
reigert1986 <85256672+reigert1986@users.noreply.github.com>
2022-10-04 18:44:14 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,62 +1,146 @@
 console.clear();
 
 const contador = document.querySelector(".contador");
+const primerPartido = document.querySelector(".primerPartido");
+const segundoPartido = document.querySelector(".segundoPartido");
+const tercerPartido = document.querySelector(".tercerPartido");
 
+console.log(segundoPartido)
+console.log(tercerPartido)
 /* 
- * FECHA DEL KARAOKE*/
-let date = new Date("2022/10/13 21:30:00").getTime();
-//let date = new Date("2022/10/03 17:35:00").getTime();
-/*
-setInterval(() =>{
-	
-	let fechaActual = new Date().getTime();
-	let restarDias = date - fechaActual;
-	
-	let days = Math.floor(restarDias / (1000 * 60 * 60 * 24));
-	let hours = Math.floor((restarDias % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));	
-	let minutes = Math.floor((restarDias % (1000 * 60 * 60)) / (1000 * 60));	
-	let seconds = Math.floor((restarDias % (1000 * 60)) / 1000);
-	
-	contador.innerHTML = `<span> Faltan: <br>
-			<p>Dias: ${days} </p>
-			<p>Horas: ${('0' + hours).slice(-2)} </p> 
-			<p>Minutos: ${('0' + minutes).slice(-2)} </p>
-			<p>Segundos: ${('0'+seconds).slice(-2)} </p>
-			
-							</span>`;
-	console.log(typeof seconds)
-							
-	if(seconds === 0){
-		contador.innerHTML = `<h2>QUE COMIENCE LA FIESTA. A CANTAR!!!</h2> `;
-	}
-							
-},1000); 
- 
-*/
+ * FECHA DEL Mundial*/
 
-let conteo = setInterval(retroceso);
-let seconds = null;
+let date = new Date("2022/11/20 12:00:00").getTime();
+/* FECHA DEL los partidos de Uruguay*/
+/*vs Corea del Sur*/
+let vsCorea = new Date("2022/11/24 10:00:00").getTime();
+/*vs Portugal*/
+let vsPortugal = new Date("2022/11/28 16:00:00").getTime();
+/*vs Ghana*/
+let vsGhana = new Date("2022/12/02 12:00:00").getTime();
+
+/* Elegir el partido */
+
+/**/
+function partido1(){
+    
+    let conteoCorea = setInterval(()=>{
+    let fechaActual = new Date().getTime();
+	let restarDias = vsCorea - fechaActual; 
+     /*funcion obtener: dia , fecha, hora, minutos, segundos*/
+    function obtenerDFHMS(){
+        let days = Math.floor(restarDias / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((restarDias % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));	
+        let minutes = Math.floor((restarDias % (1000 * 60 * 60)) / (1000 * 60));	
+        let seconds = Math.floor((restarDias % (1000 * 60)) / 1000);
+        function mostar(){
+            primerPartido.innerHTML = `<span> <h4>Falta Para el comienzo:</h4> <br>
+                <p class="letracartel">Dias: ${days} </p>
+                <p class="letracartel">Horas: ${('0' + hours).slice(-2)} </p> 
+                <p class="letracartel">Minutos: ${('0' + minutes).slice(-2)} </p>
+                <p class="letracartel">Segundos: ${('0'+seconds).slice(-2)} </p>
+            
+                            </span>`; 
+        }
+        mostar();
+    }
+
+    
+    obtenerDFHMS()
+
+    });
+}
+
+function partido2(){
+    
+    let conteoPortugal = setInterval(()=>{
+    let fechaActual = new Date().getTime();
+	let restarDias = vsPortugal - fechaActual; 
+     /*funcion obtener: dia , fecha, hora, minutos, segundos*/
+    function obtenerDFHMS(){
+        let days = Math.floor(restarDias / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((restarDias % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));	
+        let minutes = Math.floor((restarDias % (1000 * 60 * 60)) / (1000 * 60));	
+        let seconds = Math.floor((restarDias % (1000 * 60)) / 1000);
+        function mostar(){
+            segundoPartido.innerHTML = `<span> <h4>Falta Para el comienzo:</h4> <br>
+                <p class="letracartel">Dias: ${days} </p>
+                <p class="letracartel">Horas: ${('0' + hours).slice(-2)} </p> 
+                <p class="letracartel">Minutos: ${('0' + minutes).slice(-2)} </p>
+                <p class="letracartel">Segundos: ${('0'+seconds).slice(-2)} </p>
+            
+                            </span>`; 
+        }
+        mostar();
+    }
+
+    
+    obtenerDFHMS()
+
+    });
+}
+
+function partido3(){
+    
+    let conteoGhana = setInterval(()=>{
+    let fechaActual = new Date().getTime();
+	let restarDias = vsGhana - fechaActual; 
+     /*funcion obtener: dia , fecha, hora, minutos, segundos*/
+    function obtenerDFHMS(){
+        let days = Math.floor(restarDias / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((restarDias % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));	
+        let minutes = Math.floor((restarDias % (1000 * 60 * 60)) / (1000 * 60));	
+        let seconds = Math.floor((restarDias % (1000 * 60)) / 1000);
+        function mostar(){
+            tercerPartido.innerHTML = `<span> <h4>Falta Para el comienzo:</h4> <br>
+                <p class="letracartel">Dias: ${days} </p>
+                <p class="letracartel">Horas: ${('0' + hours).slice(-2)} </p> 
+                <p class="letracartel">Minutos: ${('0' + minutes).slice(-2)} </p>
+                <p class="letracartel">Segundos: ${('0'+seconds).slice(-2)} </p>
+            
+                            </span>`; 
+        }
+        mostar();
+    }
+
+    
+    obtenerDFHMS()
+
+    });
+}
+/**/
+
+
+partido1()
+partido2()
+partido3()
+
+
+let conteo = setInterval(retroceso); /*conteo del Mundial*/
 function retroceso(){
+   
 	let fechaActual = new Date().getTime();
-	let restarDias = date - fechaActual;
-	console.log(restarDias)
+	let restarDias = date - fechaActual; // date se remplaza por fecha
 	
-	let days = Math.floor(restarDias / (1000 * 60 * 60 * 24));
-	let hours = Math.floor((restarDias % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));	
-	let minutes = Math.floor((restarDias % (1000 * 60 * 60)) / (1000 * 60));	
-	let seconds = Math.floor((restarDias % (1000 * 60)) / 1000);
 	
-	contador.innerHTML = `<span> Faltan: <br>
-			<p>Dias: ${days} </p>
-			<p>Horas: ${('0' + hours).slice(-2)} </p> 
-			<p>Minutos: ${('0' + minutes).slice(-2)} </p>
-			<p>Segundos: ${('0'+seconds).slice(-2)} </p>
+        let days = Math.floor(restarDias / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((restarDias % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));	
+        let minutes = Math.floor((restarDias % (1000 * 60 * 60)) / (1000 * 60));	
+        let seconds = Math.floor((restarDias % (1000 * 60)) / 1000);
+    
+	
+	
+	contador.innerHTML = `<span> <h2>Falta Para el comienzo:</h2> <br>
+			<p class="letracartel">Dias: ${days} </p>
+			<p class="letracartel">Horas: ${('0' + hours).slice(-2)} </p> 
+			<p class="letracartel">Minutos: ${('0' + minutes).slice(-2)} </p>
+			<p class="letracartel">Segundos: ${('0'+seconds).slice(-2)} </p>
 			
 							</span>`;
 							
 	if(restarDias < 0){
 		clearInterval(conteo);
-		contador.innerHTML = `<h2>QUE COMIENCE LA FIESTA. A CANTAR!!!</h2> `;
+		contador.innerHTML = `<h2>Lets PLay a Jugar!!!!</h2> `;
 	}
 }
 
@@ -64,19 +148,5 @@ function retroceso(){
 
 
 
-//Funcion para convertir segundos a minutos y horas
-/*
-function segundosAString(segundos) {
-	var hora="";
-	if (segundos>3600){
-		hora = Math.floor(segundos / 3600);
-		hora = (hora < 10)? '0' + hora : hora;
-		hora+=":"
-	}
-	var minutos = Math.floor((segundos / 60) % 60);
-	minutos = (minutos < 10)? '0' + minutos : minutos;
-	var segundos = segundos % 60;
-	segundos = (segundos < 10)? '0' + segundos : segundos;
-	return hora  + minutos + ':' + segundos;
-}
-*/
+
+
